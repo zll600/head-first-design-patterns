fb96cf6c94dcca296d8a01f28dfe484b64d618c5
zll600 <3400692417@qq.com>
2022-07-12 16:22:07 +0800

    Add WeatherData class implementation

---
+++ src/headfirst/observer/weather/WeatherData.java
@@ -0,0 +1,41 @@
+package headfirst.observer.weather;
+
+public class Weather implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) {
+            observers.remove(i);
+        }
+    }
+
+    public void notifyObservers() {
+        for (int i = 0; i < observers.size(); i++) {
+            Observer observer = (Observer)observers.get(i);
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void messurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        messurementsChanged();
+    }
+}
