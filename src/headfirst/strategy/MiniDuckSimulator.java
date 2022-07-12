d33981a544a01922884704ee7dcd90fd520f001d
zll600 <3400692417@qq.com>
2022-07-12 14:49:18 +0800

    complete simplest strategy pattern

---
+++ src/headfirst/strategy/MiniDuckSimulator.java
@@ -0,0 +1,9 @@
+package headfirst.strategy;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performFly();
+        mallard.performQuack();
+    }
+}
