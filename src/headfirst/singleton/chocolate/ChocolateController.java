4c60868245e2727bea13581744e6a3ccb12e7115
zll600 <3400692417@qq.com>
2022-07-30 16:16:40 +0800

    Add Simple Singleton Pattern implementation

---
+++ src/headfirst/singleton/chocolate/ChocolateController.java
@@ -0,0 +1,12 @@
+package headfirst.singleton.chocolate;
+
+public class ChocolateController {
+    public static void main(String[] args) {
+        ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+        boiler.fill();
+        boiler.boil();
+        boiler.drain();
+
+        ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+    }
+}
